a3471f5ec545416c28c76a448e8b491f0ecf0509
ElizabeteTavares <elizabetetavareseng@gmail.com>
2023-02-09 16:11:49 -0300

    atualização da pasta TemaRepository

--- repository/TemaRepository.java
+++ repository/TemaRepository.java
@@ -3,8 +3,11 @@ package com.blogPessoalItau.repository;
 import org.springframework.data.jpa.repository.JpaRepository;
 import com.blogPessoalItau.model.Tema;
 import org.springframework.data.repository.query.Param;
+import org.springframework.stereotype.Repository;
+
 import java.util.List;
 
+@Repository
 public interface TemaRepository extends JpaRepository<Tema, Long> {
     public List<Tema> findAllByDescricaoContainingIgnoreCase(@Param("descricao") String descricao);
 
